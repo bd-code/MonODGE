124f0d3360084133d089399e02a7730ba4249583
bdcode <bdclemens@pm.me>
2024-11-09 12:59:30 -0500

    Added SelectedIndex property and constructor parameter to SelectWheel.

--- Source/MonODGE.UI/Components/SelectWheel.cs
+++ Source/MonODGE.UI/Components/SelectWheel.cs
@@ -13,14 +13,35 @@ namespace MonODGE.UI.Components {
     /// <typeparam name="T">Any object easily identified via its ToString() method.</typeparam>
     public class SelectWheel<T> : OdgeWheel {
         private T[] _options;
+
+        /// <summary>
+        /// The index of the selected option.
+        /// </summary>
+        public int SelectedIndex {
+            get => _selectedIndex;
+            set {
+                if (value < 0 || value >= _options.Length) {
+                    string e = $"SelectWheel: SelectedIndex = {value} while options.Length = {_options.Length};";
+                    throw new ArgumentOutOfRangeException(e);
+                }
+                _selectedIndex = value;
+            }
+        }
         private int _selectedIndex;
+
 
         public T Value => _options[_selectedIndex];
         protected override string ValueToString => Value.ToString();
 
 
-        public SelectWheel(StyleSheet style, T[] options) : base(style) {
+        public SelectWheel(StyleSheet style, T[] options, int selectedOption = 0) : base(style) {
             _options = options;
+            if (selectedOption < 0 || selectedOption >= options.Length) {
+                string e = $"SelectWheel: selectedOption = {selectedOption} while options.Length = {options.Length};";
+                throw new ArgumentOutOfRangeException(nameof(selectedOption), e);
+            }
+            _selectedIndex = selectedOption;
+
             Layout();
             Size = new Point(MinWidth, MinHeight);
         }
